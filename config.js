592e90565a64909f44a4ad73b7649c7cc1eb1062
IlyaSigma111 <ilyagulkov25@gmail.com>
2026-03-11 21:40:28 +0500

    Update config.js

--- config.js
+++ config.js
@@ -6,6 +6,7 @@
 // ═══════════════════════════════════════════════════
 // 1. НАСТРОЙКИ ПОДКЛЮЧЕНИЯ К SUPABASE
 // ═══════════════════════════════════════════════════
+// ВАЖНО: Используйте anon/public ключ, который начинается с "eyJ..."
 const SB_URL = 'https://gluqnmznmjvlyguzkdrn.supabase.co';
 const SB_KEY = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImdsdXFubXpubWp2bHlndXprZHJuIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDE2MzU5MjAsImV4cCI6MjA1NzIxMTkyMH0.2NgsE4dA-SL1RNCzEf0T79Tk9O3OEMLFJwMh7iyfJXo';
 
